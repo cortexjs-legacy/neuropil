78cef5b40557897988534b0301ee17498faf7fec
Kael <i@kael.me>
2013-07-12 17:16:57 +0800

    add default setting of logger to test/neuropil

--- test/lib/neuropil.js
+++ test/lib/neuropil.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var neuropil = require('neuropil');
+var node_url = require('url');
 
 module.exports = neuropil({
     logger: require('./logger'),
@@ -11,4 +12,27 @@ module.exports = neuropil({
 
     port: 80,
     host: 'registry.npm.lc'
+
+}).on('request', function(e) {
+    this.logger.info(
+        'CTX', 
+        this.logger.template('{{magenta method}} {{url}}', {
+            url     : e.safe_url,
+            method  : e.method
+        }) 
+    );
+
+}).on('response', function(e){
+    this.logger.info(
+        'CTX',
+        e.err ? '{{red ' + (e.res.statusCode || 'ERR') + '}}' : '{{green ' + (e.res.statusCode || 'OK!') + '}}',
+        e.req.safe_url
+    );
+
+    this.logger.debug(
+        '{{magenta ' + e.req.method + '}}',
+        node_url.parse(e.req.safe_url).pathname,
+        e.err,
+        e.body
+    );
 });
